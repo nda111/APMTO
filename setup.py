14557e68f3f60661080e1b7c7a19f152904f29a6
Yu Geunhyeok <nda111@naver.com>
2024-06-26 11:05:09 +0900

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="apmto-nda111", 
+    name="apmto", 
     version="0.0.1",
     author="Geunhyeok Yu",
     author_email="geunhyeok0111@gmail.com",
